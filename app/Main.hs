8111f320ee24e168e5cc1c4a29797cd5dfbbb233
Faustin Date <datefaustin@gmail.com>
2021-10-20 15:16:53 +0900

    Can visualize all rules in module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,73 @@
 module Main where
 
-import Lib
+import GHC.Paths (libdir)
+import DynFlags (defaultFatalMessager, defaultFlushOut)
+import Control.Monad (when)
+import System.FilePath
+import System.Posix.Files (fileExist, removeLink)
+import Options.Applicative
+import Outputable (ppr, showSDocUnsafe)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Rules (rulesOfBinds)
+
+
+import Lib (compileToCoreModule, CoreModule(..), defaultErrorHandler, getSessionDynFlags, setSessionDynFlags, runGhc)
+
+
+newtype CmdArgs = CmdArgs
+    { filename :: FilePath }
+
+
+cmdArgs :: Parser CmdArgs
+cmdArgs = CmdArgs
+    <$> argument str
+        ( metavar "FILENAME"
+        <> help "Path to Haskell program file")
+
 
 main :: IO ()
-main = putStrLn "Hello World !"
+main = compile =<< execParser opts
+    where 
+        opts = info (cmdArgs <**> helper)
+          ( fullDesc
+            <> progDesc "Output all rewrite rules in FILENAME to stdout"
+            <> header 
+            "up-to-core -- A single module compiler for Haskell"
+         )
+
+
+
+compile :: CmdArgs -> IO ()
+compile (CmdArgs inputfile) = defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+    inputfileExists <- fileExist inputfile
+    
+    cleanArtifacts inputfile
+
+    if not inputfileExists 
+       then do
+           putStrLn $ "File not found :" ++ show inputfile
+           return ()
+       else
+           runGhc (Just libdir) $ do
+               dflags <- getSessionDynFlags
+               setSessionDynFlags dflags
+               coreM <- compileToCoreModule inputfile
+               let binders = cm_binds coreM
+                   rules_of_bndrs = rulesOfBinds binders
+                   rules_of_guts  = cm_rules coreM
+
+               liftIO $ putStrLn $ showSDocUnsafe(ppr (rules_of_bndrs ++ rules_of_guts))
+               return ()
+
+    cleanArtifacts inputfile
+
+
+cleanArtifacts inputfile =
+    let oFile = replaceExtension inputfile ".o"
+        hiFile = replaceExtension inputfile ".hi"
+     in do
+         has_ofile <- fileExist oFile
+         has_hifile <- fileExist hiFile
+
+         when has_ofile (removeLink oFile)
+         when has_hifile (removeLink hiFile)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,101 @@
 module Lib
-    ( someFunc
+    ( compileToCoreModule, 
+      CoreModule (..),
+      module GHC
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import GHC hiding (compileToCoreModule, compileCore, compileToCoreSimplified, CoreModule(..))
+import HscTypes (typeEnvFromEntities, msHsFilePath, TypeEnv, CgGuts(..), ModDetails(..), ModGuts(..))
+import CoreSyn (CoreProgram, CoreRule, bindersOfBinds)
+import Panic (panic)
+import TidyPgm (tidyProgram)
+import HscMain (hscSimplify)
+import TcRnTypes (tcg_th_coreplugins, TcGblEnv)
+
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad (liftM)
+import Data.Foldable (find)
+import Data.IORef (readIORef)
+
+
+-- | A CoreModule consists of just the fields of a 'ModGuts' that are needed for
+-- the 'GHC.compileToCoreModule' interface.
+data CoreModule
+  = CoreModule {
+      -- | Module name
+      cm_module   :: !Module,
+      -- | Type environment for types declared in this module
+      cm_types    :: !TypeEnv,
+      -- | Declarations
+      cm_binds    :: CoreProgram,
+      -- | Safe Haskell mode
+      cm_safe     :: SafeHaskellMode,
+
+      cm_rules :: ![CoreRule]          -- ^ Before the core pipeline starts, contains
+                                       -- See Note [Overall plumbing for rules] in Rules.hs
+      
+    }
+
+
+compileToCoreModule :: GhcMonad m => FilePath -> m CoreModule
+compileToCoreModule = compileCore False
+
+-- | Like compileToCoreModule, but invokes the simplifier, so
+-- as to return simplified and tidied Core.
+compileToCoreSimplified :: GhcMonad m => FilePath -> m CoreModule
+compileToCoreSimplified = compileCore True
+
+compileCore :: GhcMonad m => Bool -> FilePath -> m CoreModule
+compileCore simplify fn = do
+   -- First, set the target to the desired filename
+   target <- guessTarget fn Nothing
+   addTarget target
+   _ <- load LoadAllTargets
+   -- Then find dependencies
+   modGraph <- depanal [] True
+   case find ((== fn) . msHsFilePath) (mgModSummaries modGraph) of
+     Just modSummary -> do
+       -- Now we have the module name;
+       -- parse, typecheck and desugar the module
+       (tcg, mod_guts) <- -- TODO: space leaky: call hsc* directly?
+         do tm <- typecheckModule =<< parseModule modSummary
+            let tcg = fst (tm_internals_ tm)
+            (,) tcg . coreModule <$> desugarModule tm
+       liftM (gutsToCoreModule (mg_safe_haskell mod_guts)) $
+         if simplify
+          then do
+             -- If simplify is true: simplify (hscSimplify), then tidy
+             -- (tidyProgram).
+             hsc_env <- getSession
+             simpl_guts <- liftIO $ do
+               plugins <- readIORef (tcg_th_coreplugins tcg)
+               hscSimplify hsc_env plugins mod_guts
+             tidy_guts <- liftIO $ tidyProgram hsc_env simpl_guts
+             return $ Left tidy_guts
+          else
+             return $ Right mod_guts
+
+     Nothing -> panic "compileToCoreModule: target FilePath not found in module dependency graph"
+  where -- two versions, based on whether we simplify (thus run tidyProgram,
+        -- which returns a (CgGuts, ModDetails) pair, or not (in which case
+        -- we just have a ModGuts.
+        gutsToCoreModule :: SafeHaskellMode
+                         -> Either (CgGuts, ModDetails) ModGuts
+                         -> CoreModule
+        gutsToCoreModule safe_mode (Left (cg, md)) = CoreModule {
+          cm_module = cg_module cg,
+          cm_types  = md_types md,
+          cm_binds  = cg_binds cg,
+          cm_safe   = safe_mode,
+          cm_rules  = [] -- Sorry Sempai, no rules left in my guts after simplification
+        }
+        gutsToCoreModule safe_mode (Right mg) = CoreModule {
+          cm_module  = mg_module mg,
+          cm_types   = typeEnvFromEntities (bindersOfBinds (mg_binds mg))
+                                           (mg_tcs mg)
+                                           (mg_fam_insts mg),
+          cm_binds   = mg_binds mg,
+          cm_safe    = safe_mode,
+          cm_rules    = mg_rules mg
+         }
+
